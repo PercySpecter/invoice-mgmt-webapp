61dcdad642aa8eade141e6263e8848bfd29f112d
Kinjal Ray <kinjalray10@gmail.com>
2020-01-01 20:49:25 +0530

    Product Add feature added

---
+++ views/scripts/productadd.js
@@ -0,0 +1,40 @@
+(async () => {
+  const token = localStorage.getItem('token');
+  if(token == null)
+  {
+    document.getElementById('main-body').innerHTML = `<span>
+                                                        Please <a href="login" style"color: #D20606;">Login<a> to Add Product
+                                                      </span>`;
+  }
+
+  else
+  {
+    document.getElementById('product-form').addEventListener('submit' , async (e) => {
+      e.preventDefault();
+      const product = {name: document.forms['product-form']['prodname'].value , price: document.forms['product-form']['price'].value};
+      const url = 'http://localhost:4000/api/products';
+      console.log(product);
+      try {
+        let response = await fetch(url , {
+          method: 'POST',
+          body: JSON.stringify(product),
+          headers: {
+            "Content-type": "application/json; charset=UTF-8",
+            "Authorization": `jwt ${token}`
+          }
+        });
+        const new_product = await response.json();
+        console.log(new_product);
+        document.getElementById('msg').innerHTML = `<span class="text-success">Product added. Product ID: ${new_product.id}<span>`;
+        clearForms();
+      }
+      catch (e) {
+        console.log(e);
+      }
+    });
+  }
+})();
+
+function clearForms() {
+  document.getElementById('product-form').reset();
+}
